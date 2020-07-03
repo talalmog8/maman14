eccb296d56c428b7a1f82cace4cf83f5ac7f6575
talalmog8 <talalmog8@gmail.com>
2020-07-03 09:15:03 -0700

    main now calls parse function

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <string.h>
 #include "assembler.h"
 
-int main(int argc, char *argv[]){    
+int main(int argc, char *argv[]){     
     char *arg;
-    FILE *file;
+    FILE *file;    
+
     printf("Reading Command Line Arguements For Program: \"%s\" Number Of Arguements: %d\n", argv[0], argc);
-    
+        
     while((arg = nextArg(argc, argv)) != NULL){        
       printf("Next Command Line Arguement: %s\n", arg);              
       if((file = openfile(arg))){
         printf("File Opened: %s\n", ((!file) ? "FALSE" : "TRUE"));
+        parse(file);
         disposefile(file);
         printf("Disposed File: \"%s.as\"\n", arg);
       }
