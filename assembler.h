3b724f0ec4792a9f453e2448d1c3026f9eb5cfcf
talalmog8 <talalmog8@gmail.com>
2020-07-03 01:50:03 -0700

    api for files .c

--- assembler.h
+++ assembler.h
@@ -1,4 +1,12 @@
 #include<stdio.h>
 
+/* command line arguements */
 char * nextArg(int amount, char* args[]);
 FILE * readFile(char *name);
+
+
+/* file */
+FILE * openfile(char *name);
+char* readline(FILE *file);
+void disposefile(FILE *file);
+void disposeline(char *linestart);
--- files.c
+++ files.c
@@ -5,19 +5,33 @@
 #define MAX_LINE_LENGTH 81 
 #define MAX_FILE_NAME_LENGTH 100
 
-FILE * readfile(char *name);
-char* readline(FILE *file);
-
-FILE* readfile(char *name){    
+/*
+    Tries to open file with specified name + ".as" postfix
+*/
+FILE *openfile(char *name)
+{
     char filename[MAX_FILE_NAME_LENGTH];     
     char *p= filename;
 
     p = strcpy(filename, name);
     p = strcat(filename, POSTFIX);
 
+    printf("Attempting To Open File. Filename: \"%s\"\n", p);
     return fopen(p, "r");
 }
 
+/*
+    Disposes file
+*/
+void disposefile(FILE *file){
+    fclose(file);    
+}
+
+/*
+    Reads until '\n' in specified file using dynamic memory allocation.
+    If allocation fails, program exits with error code 1.
+    Line length is At most 80 character without '\n'
+*/
 char* readline(FILE *file){    
     char* line = (char *)malloc(sizeof(char));
     char* start = line;
@@ -44,4 +58,11 @@ char* readline(FILE *file){
     }            
 
     return start;
+}
+
+/*
+    Dispose line created with dynamic memory allocation
+*/
+void disposeline(char *linestart){
+    free(linestart);
 }
