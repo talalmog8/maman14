8523763c1f78b4556619f5094a2e525d7bbf1522
talalmog8 <talalmog8@gmail.com>
2020-07-03 09:11:10 -0700

    enum for parsing result

--- assembler.h
+++ assembler.h
@@ -1,5 +1,16 @@
 #include<stdio.h>
 
+typedef enum {
+    FALSE,
+    TRUE    
+} bool;
+
+typedef enum {
+    CORRECT,
+    OTHER,
+    ERROR
+} parsing_result;
+
 /* command line arguements */
 char * nextArg(int amount, char* args[]);
 FILE * readFile(char *name);
@@ -10,3 +21,8 @@ FILE * openfile(char *name);
 char* readline(FILE *file);
 void disposefile(FILE *file);
 void disposeline(char *linestart);
+
+/* parsing */
+bool parse(FILE *file);
+int isLable(char * text);
+
