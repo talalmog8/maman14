5c1e9ecc169fe96cad6f26e682b49bfe5cffc67a
talalmog8 <talalmog8@gmail.com>
2020-07-11 13:05:05 -0700

    parses lable for reserved assembly keywords

--- cmake-build-debug/test2.as
+++ cmake-build-debug/test2.as
@@ -9,7 +9,7 @@ mov r2, #2
                  
 mov r2, #2
 ; dfsdf
-
+ 
 
 
                 
--- lable.c
+++ lable.c
@@ -1,42 +1,59 @@
 #include "assembler.h"
+
 #include <string.h>
 #include <ctype.h>
 
-/*
-    Gets text that could contain a lable. 
-    Don't send null or empty text
-    Returns 0 if lable is recognized an validated, else returns error code indicating what went wrong
-    codes:
-    0 = success
-    1 = first character is not a letter
-    2 = not containing ':'
-    3 = additional text after ':' perhaps missing white character after lable or illegal lable characters 
-    4 = illegal lable characters
-*/
-int isLable(char * text){
-    int i = 0;             
-    int result = 1; /* first character is not a letter */
-    
-    if(isalpha(text[i])){                
-        while (isalnum(text[i]))
-        {
-            i++;                     
-        }       
-        if(text[i] == '\0'){            
-            result = 2; /* no a lable no containing : */
-        }
-        else if(text[i] == ':'){
-            if(text[i + 1] == '\0'){
-                result = 0; /* legit lable*/
-            }
-            else{
-                result = 3; /* additional text after ':' perhaps missing white character after lable or illegal lable characters */
-            }
-        }        
-        else{
-            result = 4; /* illegal lable characters */
-        }
+#define MAX_LABLE_SIZE 31
+#define RESERVED_OPS_AMOUNT 11
+
+
+char *findlable(char *text) {
+    int i = 0;
+
+    while (text[i] != '\n' && text[i] != ':') {
+        i++;
     }
 
-    return result;
+    if (text[i] == ':') {
+        return (text + i + 1);
+    }
+
+    return NULL;
 }
+
+bool parselable(char *lable, int length) {
+    int i;
+
+    if (length > MAX_LABLE_SIZE) {
+        fprintf(stderr, "Lable bigger than max size\n");
+        return FALSE;
+    }
+    if (!isalpha(lable[0])) {
+        fprintf(stderr, "Lable not starting with letter\n");
+        return FALSE;
+    }
+
+    struct {
+        char *name;
+    } reserved[] = {
+            {"r1"},
+            {"r2"},
+            {"r3"},
+            {"r4"},
+            {"r5"},
+            {"r6"},
+            {"r7"},
+            {".data"},
+            {".string"},
+            {".entry"},
+            {".extern"},
+    };
+
+    for (i = 0; i < RESERVED_OPS_AMOUNT; ++i) {
+        if (!strncmp(lable, reserved[i].name, length)) {
+            fprintf(stderr, "reserved assembly word used as label %s\n", reserved[i].name);
+            return FALSE;
+        }
+    }
+}
+
