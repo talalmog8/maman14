9379f9f39fd672ecaf6f1dae5e15d52885bffb23
talalmog8 <talalmog8@gmail.com>
2020-08-06 14:35:30 -0700

    refactoring

--- operations_parsers.c
+++ operations_parsers.c
@@ -16,12 +16,19 @@
 #define REGISTER_ADDRESSING 3
 
 static int parse_one_arg_command(char *text, int opcode, int funct);
-static void fill_one_arg_command_defaults(command_template *command, int opcode, int funct, int des_register, int des_delivery_type);
+
+static void fill_one_arg_command_defaults(command_template *command, int opcode, int funct);
+
 static void insert_number_to_command(command_template *command, int number);
+
 static bool parse_zero_args_command(char *text, int opcode, int funct);
+
 static void dispose_operands(arguments args);
+
 static int parse_arg(char *arg, command_template *, bool);
+
 static int parse_two_args_command(char *text, int opcode, int funct);
+
 static void fill_empty_command(command_template *command);
 
 int parse_mov(char *text, int opcode, int funct) {
@@ -47,16 +54,14 @@ int parse_lea(char *text, int opcode, int funct) {
 int parse_clr(char *text, int opcode, int funct) {
     int parsed, delivery_type;
 
-    parsed =  parse_one_arg_command(text, opcode, funct);
-    delivery_type = peek_last_command() -> des_delivery_type;
+    parsed = parse_one_arg_command(text, opcode, funct);
+    delivery_type = peek_last_command()->des_delivery_type;
 
-    if(!parsed){
-        return  FALSE;
-    }
-    else if(delivery_type == 1 || delivery_type == 3){
+    if (!parsed) {
+        return FALSE;
+    } else if (delivery_type == 1 || delivery_type == 3) {
         return TRUE;
-    }
-    else{
+    } else {
         fprintf(stderr, "Not a valid delivery type fo clr command. delivery type: %d", delivery_type);
     }
 }
@@ -94,47 +99,43 @@ int parse_prn(char *text, int opcode, int funct) {
 }
 
 int parse_rts(char *text, int opcode, int funct) {
-    return  parse_zero_args_command(text, opcode, funct);
+    return parse_zero_args_command(text, opcode, funct);
 }
 
 int parse_stop(char *text, int opcode, int funct) {
     return parse_zero_args_command(text, opcode, funct);
 }
 
-static void dispose_operands(arguments args){
-    if(args.arg1 != NULL && args.arg2 != NULL){
+static void dispose_operands(arguments args) {
+    if (args.arg1 != NULL && args.arg2 != NULL) {
         free(args.arg1);
         free(args.arg2);
-    }
-    else if(args.arg1 != NULL){
+    } else if (args.arg1 != NULL) {
         free(args.arg1);
-    }
-    else if(args.arg2 != NULL){
+    } else if (args.arg2 != NULL) {
         free(args.arg2);
     }
 }
 
-static int parse_two_args_command(char *text, int opcode, int funct){
+static int parse_two_args_command(char *text, int opcode, int funct) {
     int parsed = 0;
     arguments args = read_args(text);
     command_template *command = get_current_command();
 
-    if(args.arg1 && args.arg2){
-        command -> opcode = opcode;
-        command -> func = funct;
-        command -> A = 1;
-        command -> R = 0;
-        command -> E  = 0;
+    if (args.arg1 && args.arg2) {
+        command->opcode = opcode;
+        command->func = funct;
+        command->A = 1;
+        command->R = 0;
+        command->E = 0;
 
-        if(parse_arg(args.arg1, command, FALSE) != -1){
-            if(parse_arg(args.arg2, command, TRUE) != -1){
+        if (parse_arg(args.arg1, command, FALSE) != -1) {
+            if (parse_arg(args.arg2, command, TRUE) != -1) {
                 parsed = 1;
-            }
-            else{
+            } else {
                 fprintf(stderr, "Failed to parse second argument in command. arguments: %s", text);
             }
-        }
-        else{
+        } else {
             fprintf(stderr, "Failed to parse first argument in command. arguments: %s", text);
         }
 
@@ -144,131 +145,124 @@ static int parse_two_args_command(char *text, int opcode, int funct){
     return parsed;
 }
 
-static int parse_arg(char *arg, command_template *command, bool is_destination){
+static int parse_arg(char *arg, command_template *command, bool is_destination) {
     int number_arg;
     int _register = EMPTY_FIELD;
     int addressing_type = -1;
 
     if ((_register = isregister(arg)) != -1) {
         addressing_type = REGISTER_ADDRESSING;
-    }
-    else if(try_parse_number(arg,&number_arg)){
+    } else if (try_parse_number(arg, &number_arg)) {
         addressing_type = IMMEDIATE_ADDRESSING;
         insert_number_to_command(get_current_command(), number_arg);
-    }
-    else if(isaddress(arg)){
-        addressing_type =  RELATIVE_ADDRESSING;
+    } else if (isaddress(arg)) {
+        addressing_type = RELATIVE_ADDRESSING;
         fill_empty_command(get_current_command()); /* reserved for label's address */
         incIC(1);
-    }
-    else if (islable(arg, strlen(arg))) {
+    } else if (islable(arg, strlen(arg))) {
         addressing_type = DIRECT_ADDRESSING;
         fill_empty_command(get_current_command()); /* reserved for label's address */
         incIC(1); /* saved space for label address*/
     }
 
 
-    if(is_destination){
-        command -> des_delivery_type = addressing_type;
-        command -> des_register = (_register == -1) ? EMPTY_FIELD : _register;
-    }
-    else{
-        command -> orig_delivery_type = addressing_type;
-        command -> orig_register = (_register == -1) ? EMPTY_FIELD : _register;
+    if (is_destination) {
+        command->des_delivery_type = addressing_type;
+        command->des_register = (_register == -1) ? EMPTY_FIELD : _register;
+    } else {
+        command->orig_delivery_type = addressing_type;
+        command->orig_register = (_register == -1) ? EMPTY_FIELD : _register;
     }
 
     return addressing_type;
 }
 
+static void fill_one_arg_command_defaults(command_template *command, int opcode, int funct) {
+    command->opcode = opcode;
+    command->func = funct;
+    command->A = 1;
+    command->R = 0;
+    command->E = 0;
+    command->orig_register = EMPTY_FIELD;
+    command->orig_delivery_type = EMPTY_FIELD;
+    incIC(1);
+}
+
 static int parse_one_arg_command(char *text, int opcode, int funct) {
     int _register, number_arg;
-    int parsed = 0;
-    command_template *command;
-    char *arg = read_arg(text);
+    int addressing_type = -1;
+    command_template *command = get_current_command();
+    char *arg;
 
-    if (arg) {
-        command = get_current_command();
-        if ((_register = isregister(arg)) != -1) {
-            fill_one_arg_command_defaults(command, opcode, funct, _register, 3);
-            parsed = 1;
-        }
-        else if(try_parse_number(arg,&number_arg)){
-            fill_one_arg_command_defaults(command, opcode, funct, 0, 0);
-            insert_number_to_command(get_current_command(), number_arg);
-            parsed = 1;
-        }
-        else if(isaddress(arg)){
-            fill_one_arg_command_defaults(command, opcode, funct, 0, 2);
-            fill_empty_command(get_current_command()); /* reserved for label's address */
-            parsed = 1;
-        }
-        else if (islable(arg, strlen(arg))) {
-            fill_one_arg_command_defaults(command, opcode, funct, 0, 1);
-            fill_empty_command(get_current_command());
-            incIC(1); /* saved space for label address*/
-            parsed = 1;
-        }
-        free(arg);
+    if (!(arg = read_arg(text))){
+        return -1;
     }
 
-    return parsed;
+    fill_one_arg_command_defaults(command, opcode, funct);
+    if ((_register = isregister(arg)) != -1) {
+        command->des_register = _register;
+        command->des_delivery_type = addressing_type = REGISTER_ADDRESSING;
+    } else if (try_parse_number(arg, &number_arg)) {
+        command->des_register = EMPTY_FIELD;
+        command->des_delivery_type = addressing_type = IMMEDIATE_ADDRESSING;
+        insert_number_to_command(get_current_command(), number_arg);
+    } else if (isaddress(arg)) {
+        command->des_register = EMPTY_FIELD;
+        command->des_delivery_type = addressing_type = RELATIVE_ADDRESSING;
+        fill_empty_command(get_current_command()); /* reserved for label's address */
+        incIC(1);
+    } else if (islable(arg, strlen(arg))) {
+        command->des_register = EMPTY_FIELD;
+        command->des_delivery_type = addressing_type = DIRECT_ADDRESSING;
+        fill_empty_command(get_current_command());
+        incIC(1); /* saved space for label address*/
+    }
+    free(arg);
+
+    return addressing_type;
 }
 
-static void fill_one_arg_command_defaults(command_template *command, int opcode, int funct, int des_register, int des_delivery_type){
+static bool parse_zero_args_command(char *text, int opcode, int funct) {
+    command_template *command = get_current_command();
     command->opcode = opcode;
     command->func = funct;
-    command->orig_register = 0;
-    command->orig_delivery_type = 0;
-    command->des_register = des_register;
-    command->des_delivery_type = des_delivery_type;
+    command->orig_register = EMPTY_FIELD;
+    command->orig_delivery_type = EMPTY_FIELD;
+    command->des_register = EMPTY_FIELD;
+    command->des_delivery_type = EMPTY_FIELD;
     command->A = 1;
     command->R = 0;
     command->E = 0;
     incIC(1);
-}
 
-static bool parse_zero_args_command(char *text, int opcode, int funct){
-    command_template *command;
-    command = get_current_command();
-    command->opcode = opcode;
-    command->func = funct;
-    command->orig_register = 0;
-    command->orig_delivery_type = 0;
-    command->des_register = 0;
-    command->des_delivery_type = 0;
-    command->A = 1;
-    command->R = 0;
-    command->E = 0;
-    incIC(1);
-
-    if(!is_end(*text)){
+    if (!is_end(*text)) {
         fprintf(stderr, "Excess text in command.text: %s", text);
-        return  FALSE;
+        return FALSE;
     }
     return TRUE;
 }
 
-static void insert_number_to_command(command_template *command, int number){
-    command -> opcode = EXTRACT_OPCODE(number);
-    command -> func = EXTRACT_FUNC(number);
-    command -> des_register = EXTRACT_DES_REG(number);
-    command -> des_delivery_type = EXTRACT_DES_DEV_TYPE(number);
-    command -> orig_register = EXTRACT_ORIG_REG(number);
-    command -> orig_delivery_type = EXTRACT_ORIG_DEV_TYPE(number);
-    command -> A = 1;
-    command -> R = 0;
-    command -> E = 0;
+static void insert_number_to_command(command_template *command, int number) {
+    command->opcode = EXTRACT_OPCODE(number);
+    command->func = EXTRACT_FUNC(number);
+    command->des_register = EXTRACT_DES_REG(number);
+    command->des_delivery_type = EXTRACT_DES_DEV_TYPE(number);
+    command->orig_register = EXTRACT_ORIG_REG(number);
+    command->orig_delivery_type = EXTRACT_ORIG_DEV_TYPE(number);
+    command->A = 1;
+    command->R = 0;
+    command->E = 0;
     incIC(1);
 }
 
-static void fill_empty_command(command_template *command){
-    command -> opcode = 0;
-    command -> des_register = 0;
-    command -> des_delivery_type = 0;
-    command -> orig_register = 0;
-    command -> orig_delivery_type = 0;
-    command -> A = 0;
-    command -> R = 0;
-    command -> E = 0;
-    command -> func = 0;
+static void fill_empty_command(command_template *command) {
+    command->opcode = 0;
+    command->des_register = 0;
+    command->des_delivery_type = 0;
+    command->orig_register = 0;
+    command->orig_delivery_type = 0;
+    command->A = 0;
+    command->R = 0;
+    command->E = 0;
+    command->func = 0;
 }
