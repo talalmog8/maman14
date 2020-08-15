1d7401e9b51183edf2e755ed15e943b6710c6c4e
talalmog8 <37555109+talalmog8@users.noreply.github.com>
2020-08-15 18:28:03 +0300

    api

--- firstpass.c
+++ firstpass.c
@@ -1,13 +1,18 @@
 #include "assembler.h"
 
+/*
+    Performs the firstpass of assembler on current source file
+*/
 bool firstpass(FILE *file) {
-    bool output = TRUE;
-    bool foundLabel;
-    char *moving_line, *label;
-    int label_length;
-    int guide_result = 0;
-    char line_mem[MAX_LINE_LENGTH];
+    bool output = TRUE; /* will be FALSE if there is an error in firstpass */
+    bool foundLabel; /* TRUE if label is found*/
+    char *moving_line; /* holds current position in the line that is being parsed */
+    char *label; /* points to label found */
+    int label_length; /* holds label's length */
+    int guide_result = 0;  /*  */
+    char line_mem[MAX_LINE_LENGTH]; /* reused for holding current line read from source file */
     
+    /* intializes registers */
     setIC(100);
     setDC(0);    
     while (readline(file, (moving_line = line_mem)) != NULL) {
@@ -17,10 +22,12 @@ bool firstpass(FILE *file) {
         if (is_comment_or_empty(moving_line)) {
             continue;
         } 
-        else if ((label_length = findlable(moving_line, TRUE)) != -1) {            
+        else if ((label_length = findlable(moving_line, TRUE)) != -1) {   
+            /* found label's delimeter at beginning od command */         
             if (parselable(moving_line, label_length, (label = allocate_label(label_length)))) {
+                /* label has been validated successfuly */
                 foundLabel = TRUE;
-                skip_characters(&moving_line, label_length + 1);
+                skip_characters(&moving_line, label_length + 1); /* foward line after the label*/
             } 
             else {
                 fprintf(stderr, "Found illegal label in line: %s", line_mem);
@@ -29,12 +36,13 @@ bool firstpass(FILE *file) {
         }
 
         if (((guide_result = is_guide(&moving_line)) == __string) || guide_result == __data) {
-            if (foundLabel == TRUE)
-                output &= add_label_if_new(label, getDC(), label_data, label_no_kind);
-            output &= parse_guide(moving_line, guide_result);
+            /* found a .string or .data operation */
+            if (foundLabel == TRUE) 
+                output &= add_label_if_new(label, getDC(), label_data, label_no_kind); /* add label to symbol table */
+            output &= parse_guide(moving_line, guide_result); /* parse the rest of guide command */
         }
         else if (guide_result == __entry) {
-            continue;
+            continue; /* parsed at secondpass */
         }
         else if (guide_result == __extern) {            
             if ((label_length = findlable(moving_line, FALSE)) == -1) {
@@ -42,7 +50,8 @@ bool firstpass(FILE *file) {
                 output = FALSE;
             }
             else if (parselable(moving_line, label_length, (label = allocate_label(label_length)))) {
-                addtoend(label, 0, label_data, label_external);
+                /* found a valid label in .extern guide */
+                addtoend(label, 0, label_data, label_external); /* add label to symbol table */
                 skip_characters(&moving_line, label_length);
                 if (!is_end(*moving_line)) {
                     fprintf(stderr,"Too much data in command: %s", line_mem);
@@ -52,12 +61,12 @@ bool firstpass(FILE *file) {
         else {
             /* this should be command line*/
             if(foundLabel)
-                output &= add_label_if_new(label, getIC(), label_code, label_no_kind);
+                output &= add_label_if_new(label, getIC(), label_code, label_no_kind); /* add label to symbol table */
 
-            output &= firstpass_parse_command(&moving_line);
+            output &= firstpass_parse_command(&moving_line); /* parse the rest of the command */
         }
     }
 
-    increment_data_labels(getIC());
+    increment_data_labels(getIC()); /* updates symbols table at end of firstpass */
     return output;
 }
